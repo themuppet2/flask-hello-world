c51f1b9fd22d92332f26d35c995e0412d13960da
James Collins <j_collins@mac.com>
2016-03-30 20:06:10 +0100

    created Flask app! It's called app.py

--- app.py
+++ app.py
@@ -6,13 +6,48 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True  # IMPORTANT!!!!! NEVER USE IN
+# PRODUCTION!!!!!!!
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
-
 # define the view using a function, which returns a string
 def hello_world():
     return "Hello World!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return("correct")
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name), 200 # 200 is the status
+        # code
+    else:
+        return "Not Found", 404
+
+
+
+
 
 # start the development server using the run() method
 if __name__ == "__main__":
